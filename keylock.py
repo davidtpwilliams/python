fcf68b5095983fbf578a5226e030b564026f0d3b
David Williams <blakey247@gmail.com>
2015-11-11 14:31:21 +1100

    ch ch changes

--- keylock.py
+++ keylock.py
@@ -7,7 +7,7 @@ if (len(sys.argv) != 3) or (sys.argv[1] != '-l' and sys.argv[1] != '-u'):
    print("Usage: " + sys.argv[0] + " -[l|u] filename")
    exit(1)
 
-#Run unlock of lock
+#Run unlock or lock
 filename = sys.argv[2]
 if sys.argv[1] == '-l':
    myssl.lock(filename, "")
